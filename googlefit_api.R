f81d8f5d713a2e11ed3c90446f1bb5e63e5e8077
lotofdata <s1mpass@gmail.com>
2017-05-26 23:15:43 +0300

    kazkas veikia

--- googlefit_api.R
+++ googlefit_api.R
@@ -1,3 +1,21 @@
 library(RGoogleFit)
 library(tidyverse)
+source("API.R")
 
+
+startTime <- as.POSIXct("2017-05-24 00:00:00")
+endTime <- as.POSIXct(Sys.Date())
+
+
+
+dataset <- GetFitDataset(token, datasource, startTime, endTime)
+df$point
+
+df <- GetFitDatasources(token)
+
+df$dataStreamId
+
+datasource <- "raw:com.google.step_count.cumulative:LGE:Nexus 5X:cce4a086:BMI160 Step counter"
+
+df2 <- GetFitDatasource(token, datasource)
+df2$dataType$name
